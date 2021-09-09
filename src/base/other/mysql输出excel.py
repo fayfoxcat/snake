d8c79ed962fc3ca54cdc240784a5e039c9b8fa19
fox <fox@gamil.com>
2021-09-09 16:15:56 +0800

    操作数据，操作excel

---
+++ src/base/other/mysql输出excel.py
@@ -0,0 +1,26 @@
+from typing import List
+import pymysql
+import pandas as pd
+
+# 连接数据库
+conn = pymysql.connect(host='localhost', port=3306, user='root', password='123456', database='api', charset='utf8')
+cursor = conn.cursor()
+
+# 查询数据
+cursor.execute('SELECT * FROM `t_area`')
+# 构建list
+array: List[str] = [line for line in cursor.fetchall()]
+
+# 数据载入到 DataFrame 对象
+df = pd.DataFrame(array, columns=['序号', '省编码', '省份', '市编码', '城市', '县编码', '县'], dtype=str)
+# 返回第一行和第二行
+# print(df.loc[[0]])
+
+# 文件路径
+file: str = 'C:/Users/fayfo/Desktop/数据.xlsx'
+
+df.to_excel(excel_writer=file, sheet_name='数据', na_rep='', index=False)
+
+# 关闭游标及连接
+cursor.close()
+conn.close()

3c23236a8ef6d10a72ed98bc560eb9f140425fbb
fox <fox@gamil.com>
2021-09-27 17:43:20 +0800

    操作数据，操作excel

--- src/base/data-structure/cv.py
+++ src/base/data-structure/cv.py
@@ -1,8 +1,11 @@
-def anagramSolution1(s1, s2):
+# 清点法
+from timeit import Timer
+
+
+def anagram_by_list(s1, s2):
     alist = list(s2)
     pos1 = 0
     stillOK = True
-
     while pos1 < len(s1) and stillOK:
         pos2 = 0
         found = False
@@ -11,17 +14,87 @@ def anagramSolution1(s1, s2):
                 found = True
             else:
                 pos2 = pos2 + 1
-
         if found:
             alist[pos2] = None
         else:
             stillOK = False
-
         pos1 = pos1 + 1
-
     return stillOK
 
 
-solution_ = anagramSolution1("aabbcc", "abcabc")
+# 排序法
+def anagram_by_sort(s1, s2):
+    list1 = list(s1)
+    list2 = list(s2)
 
-print(solution_)
+    list1.sort()
+    list2.sort()
+
+    flag = True
+    for index, item in enumerate(list1):
+        if item != list2[index]:
+            flag = False
+    return flag
+
+
+# 计数法
+def anagram_by_count(s1, s2):
+    c1 = [0] * 26
+    c2 = [0] * 26
+
+    for i in range(len(s1)):
+        pos = ord(s1[i]) - ord('a')
+        c1[pos] = c1[pos] + 1
+
+    for i in range(len(s2)):
+        pos = ord(s2[i]) - ord('a')
+        c2[pos] = c2[pos] + 1
+
+    flag = True
+    i: int = 0
+    while i < 26 and flag:
+        if c1[i] == c2[i]:
+            i = i + 1
+        else:
+            flag = False
+    return flag
+
+
+# a: str = "aabbcc"
+# b: str = "abaccc"
+# print(anagram_by_list(a, b))
+# print(anagram_by_sort(a, b))
+# print(anagram_by_count(a, b))
+
+
+def generate_list_1():
+    lists = []
+    for i in range(1000):
+        lists = lists + [i]
+
+
+def generate_list_2():
+    lists = []
+    for i in range(1000):
+        lists.append(i)
+
+
+def generate_list_3():
+    lists = [i for i in range(1000)]
+
+
+def generate_list_4():
+    lists = list(range(1000))
+
+
+t1 = Timer("generate_list_1()", "from __main__ import generate_list_1")
+print("连接 ", round(t1.timeit(number=1000), 5), " 秒")
+
+t2 = Timer("generate_list_2()", "from __main__ import generate_list_2")
+print("追加 ", round(t2.timeit(number=1000), 5), " 秒")
+
+t3 = Timer("generate_list_3()", "from __main__ import generate_list_3")
+print("列表解析 ", round(t3.timeit(number=1000), 5), " 秒")
+
+t4 = Timer("generate_list_4()", "from __main__ import generate_list_4")
+print("列表构造器 ", round(t4.timeit(number=1000), 5), " 秒")

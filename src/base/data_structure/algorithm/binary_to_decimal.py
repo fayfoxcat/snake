053f4a67e84d05155516d326fc6c89e68fecfc3f
fox <fox@gamil.com>
2021-09-29 15:05:51 +0800

    栈

--- src/base/data_structure/algorithm/binary_to_decimal.py
+++ src/base/data_structure/algorithm/binary_to_decimal.py
@@ -1,21 +1,23 @@
-"""
-十进制转二进制
-"""
-from typing import List
-
 from Stack import Stack
 
+"""
+十进制转其他进制进制
+"""
 
-def solution(value: int) -> int:
-    binary: str = "0"
+
+def solution(scale: int, value: int) -> str:
+    number: str = '0123456789ABCDEF'
+    binary: str = ''
     stack = Stack()
     while value > 0:
-        stack.push(int(value % 2))
-        value = int(value / 2)
+        stack.push(int(value % scale))
+        value = int(value / scale)
 
     while stack.size() > 0:
-        binary = binary + str(stack.pop())
-    return int(binary)
+        binary = binary + number[stack.pop()]
+    return binary
 
 
-print(solution(120))
+s_scale: int = 16
+v_value: int = 1200
+print(solution(s_scale, v_value))

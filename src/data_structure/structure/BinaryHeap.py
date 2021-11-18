97a92bd5cd7d949f2e09ca34298841aa7ba116be
fox <fox@gamil.com>
2021-11-18 16:58:37 +0800

    堆结构实现

--- src/data_structure/structure/BinaryHeap.py
+++ src/data_structure/structure/BinaryHeap.py
@@ -3,6 +3,15 @@ class BinaryHeap:
         self.heap_list = [0]
         self.current_size = 0
 
+    def min_child(self, item):
+        if item * 2 + 1 > self.current_size:
+            return item * 2
+        else:
+            if self.heap_list[item * 2] < self.heap_list[item * 2 + 1]:
+                return item * 2
+            else:
+                return item * 2 + 1
+
     def perceive_up(self, i):
         while i // 2 > 0:
             if self.heap_list[i] < self.heap_list[i // 2]:
@@ -20,16 +29,23 @@ class BinaryHeap:
                 self.heap_list[item] = tmp
             item = mc
 
-    def min_child(self, item):
-        if item * 2 + 1 > self.current_size:
-            return item * 2
-        else:
-            if self.heap_list[item * 2] < self.heap_list[item * 2 + 1]:
-                return item * 2
-            else:
-                return item * 2 + 1
+    def build_heap(self, array):
+        i = len(array) // 2
+        self.current_size = len(array)
+        self.heap_list = [0] + array[:]
+        while i > 0:
+            self.perceive_down(i)
+            i = i - 1
 
     def insert(self, value):
         self.heap_list.append(value)
         self.current_size = self.current_size + 1
         self.perceive_up(self.current_size)
+
+    def delete_min(self):
+        remove_value = self.heap_list[1]
+        self.heap_list[1] = self.heap_list[self.current_size]
+        self.current_size = self.current_size - 1
+        self.heap_list.pop()
+        self.perceive_down(1)
+        return remove_value

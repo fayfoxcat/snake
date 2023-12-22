da1e0f8d24509ed46dc5dc39f50ae2317c2102e7
cat <fayfoxcat@gamil.com>
2023-12-22 14:57:32 +0800

    添加封面

--- src/base/script/pdf/pdf.py
+++ src/base/script/pdf/pdf.py
@@ -1,16 +1,19 @@
 import json
 
-from src.base.script.pdf.巡检报告 import addText, addTable, build
+from src.base.script.pdf.巡检报告 import addCover, addText, addTable, build
 
-# 示例多行数据
+with open('resources/设备信息.json', 'r', encoding='utf-8') as file:
+    deviceInfo = json.load(file)
+    addCover("resources/cover.jpg", deviceInfo)
+
 with open('resources/单板运行状态.json', 'r', encoding='utf-8') as file:
     table = json.load(file)
     body = [{'level': 1, 'content': "2 网络设备"},
             {'level': 2, 'content': "2.1 网络状态检查"},
             {'level': 3, 'content': "2.1.1 VRRP 运行状态"},
-            {'level': 4, 'content': "2.1.1.1 VRRP 运行状态"}]
+            {'level': 4, 'content': "2.1.1 VRRP 运行状态"}]
     header = "单板运行状态"
-    conditions = [{'column': 'Type', 'expression': lambda x: x > 1, 'color': "#FFA500"},
+    conditions = [{'column': 'Type', 'expression': lambda x: x is None, 'color': "#FFA500"},
                   {'column': 'Online Status', 'expression': lambda x: x < 60, 'color': "#FA5050"}]
     addText(body)
     addTable(header, table, addSubRow=True, conditions=conditions)
@@ -31,5 +34,5 @@ with open('resources/OSPF错误统计.json', 'r', encoding='utf-8') as file:
     addText(body)
     addTable(header, table, columnBold=["item"], conditions=conditions)
 
-# 调用函数生成PDFX
+# 调用函数生成PDF
 build("out/巡检报告.pdf")
--- src/base/script/pdf/巡检报告.py
+++ src/base/script/pdf/巡检报告.py
@@ -1,7 +1,7 @@
 #!/bin/env python
 # -*- coding: UTF-8 -*-
 """
-方法用于生成pdf，依次需求反复调用 addText addTable构建pdf页面，调用build生成pdf文档
+方法用于生成pdf，依次需求调用cover反复调用 addText addTable构建pdf页面，调用build生成pdf文档
 """
 import logging
 from hashlib import sha1
@@ -9,33 +9,37 @@ from typing import List
 
 import pandas as pd
 from reportlab.lib import pagesizes
-from reportlab.lib.styles import ParagraphStyle as PS, getSampleStyleSheet
-from reportlab.lib.units import mm
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.styles import ParagraphStyle, getSampleStyleSheet
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfgen.canvas import Canvas
-from reportlab.platypus import TableStyle, Paragraph, PageBreak, BaseDocTemplate, PageTemplate, Table
+from reportlab.platypus import TableStyle, Paragraph, PageBreak, PageTemplate, Table, SimpleDocTemplate, Flowable
 from reportlab.platypus.frames import Frame
 from reportlab.platypus.tableofcontents import TableOfContents
 
 # 注册字体
-pdfmetrics.registerFont(TTFont('ChineseFont', 'font/SimSun.ttf'))
+pdfmetrics.registerFont(TTFont('ChineseFont-Slim', 'font/SimSun.ttf'))
 pdfmetrics.registerFont(TTFont('ChineseFont-Bold', 'font/微软雅黑粗体.ttf'))
 
 # 目录样式
-ContentStyle = PS(name='centered', fontName='ChineseFont-Bold', fontSize=17, leftIndent=-10, leading=12, spaceAfter=10,
-                  textColor="#365F91")
+ContentStyle = ParagraphStyle(name='centered', fontName='ChineseFont-Bold', fontSize=17, leftIndent=-10, leading=12,
+                              spaceAfter=10, textColor="#365F91")
 ContentsStyle = [
-    PS(fontName='ChineseFont', fontSize=10, name='TOCHeading1', leading=12, leftIndent=-10),
-    PS(fontName='ChineseFont', fontSize=10, name='TOCHeading2', leading=12, leftIndent=0),
-    PS(fontName='ChineseFont', fontSize=10, name='TOCHeading3', leading=12, leftIndent=10)
+    ParagraphStyle(name='TOCHeading1', fontName='ChineseFont-Slim', fontSize=10, leading=12, leftIndent=-10),
+    ParagraphStyle(name='TOCHeading2', fontName='ChineseFont-Slim', fontSize=10, leading=12, leftIndent=0),
+    ParagraphStyle(name='TOCHeading3', fontName='ChineseFont-Slim', fontSize=10, leading=12, leftIndent=10)
 ]
 
 # 定义1-4级标题样式
-h1 = PS(name='Heading1', fontName='ChineseFont-Bold', fontSize=16, leftIndent=-5, spaceBefore=10, leading=20)
-h2 = PS(name='Heading2', fontName='ChineseFont-Bold', fontSize=14, leftIndent=-5, spaceBefore=10, leading=20)
-h3 = PS(name='Heading3', fontName='ChineseFont-Bold', fontSize=12, leftIndent=-5, spaceBefore=10, leading=20)
-h4 = PS(name='Heading4', fontName='ChineseFont', fontSize=12, leftIndent=-5, spaceBefore=10, leading=20)
+h1 = ParagraphStyle(name='Heading1', fontName='ChineseFont-Bold', fontSize=16, leftIndent=-20, spaceBefore=10,
+                    leading=20)
+h2 = ParagraphStyle(name='Heading2', fontName='ChineseFont-Bold', fontSize=14, leftIndent=-20, spaceBefore=10,
+                    leading=20)
+h3 = ParagraphStyle(name='Heading3', fontName='ChineseFont-Bold', fontSize=12, leftIndent=-20, spaceBefore=10,
+                    leading=20)
+h4 = ParagraphStyle(name='Heading4', fontName='ChineseFont-Slim', fontSize=12, leftIndent=-20, spaceBefore=10,
+                    leading=20)
 
 # 定义全局表格样式
 tableStyle = TableStyle([
@@ -54,61 +58,141 @@ tableStyle = TableStyle([
     ('LINEAFTER', (-1, 0), (-1, -1), 0, '#FFFFFF'),  # 设置最后一列右边框为白色
 ])
 
+# 表格条件样式：次表头样式、指定列样式、单元格样式
+styles = getSampleStyleSheet()
+subRowStyle = styles['Normal'].clone(name='subRowStyle', fontName='ChineseFont-Bold', fontSize=10)
+columnStyles = styles['Normal'].clone(name='subRowStyle', fontName='ChineseFont-Bold', fontSize=10)
+cellStyle = styles['Normal'].clone(name='cellStyle', fontName='ChineseFont-Slim', fontSize=10)
 
-# 定义页面模板样式
-class CustomTemplate(BaseDocTemplate):
+# pdf页面：封面、正文
+Cover = []
+Pages = []
+
+
+class CanvasDrawing(Flowable):
+    """ 绘制首页：一个自定义Flowable，用于在文档中绘制Canvas内容 """
+
+    def __init__(self, img_path, info):
+        Flowable.__init__(self)
+        self.img_path = img_path
+        self.info = info
+        self.width = A4[0] - 11
+        self.height = A4[1] - 12
+
+    def draw(self):
+        # 绘制图片
+        self.canv.drawImage(self.img_path, -6, 14, width=A4[0], height=A4[1])
+
+        # 绘制标题
+        header = next(iter(self.info.values()), None) + " 资源明细报告"
+        sub = next(iter(self.info.values()), None).replace("_", " ") + " RESOURCE DETAILS"
+        self.draw_custom_text(header, 0, 570, "center", "ChineseFont-Bold", 30, "#244B98")
+        self.draw_custom_text(sub, 0, 600, "center", "ChineseFont-Bold", 14, "#969897")
+        for i, item in enumerate(self.info):
+            self.draw_custom_text(item + "：" + self.info.get(item), 180, (650 + i * 30), "left",
+                                  "ChineseFont-Bold", 16, "#000000")
+
+    def draw_custom_text(self, text, x, y, align, font, size, color):
+        """
+        绘制自定义文本
+        :param text: 文本
+        :param x: 左下x
+        :param y: 左下y
+        :param align: 文本
+        :param font: 字体
+        :param size: 字体大小
+        :param color: 字体颜色
+        """
+        # 设置字体和大小
+        self.canv.setFont(font, size)
+
+        # 转换16进制颜色为RGB
+        rgb_color = self.hex_to_rgb(color)
+        self.canv.setFillColorRGB(*rgb_color)
+
+        # 计算文本宽度
+        text_width = self.canv.stringWidth(text, font, size)
+
+        # 根据对齐方式调整x位置
+        if align == "center":
+            x += (self.width - text_width) / 2
+        elif align == "right":
+            x += text_width
+        # 绘制文本
+        self.canv.drawString(x, self.height - y, text)
+
+    @staticmethod
+    def hex_to_rgb(hex_color):
+        """
+        将16进制颜色转换为RGB格式
+        :param hex_color: 16进制色
+        :return: RGB色
+        """
+        hex_color = hex_color.lstrip('#')
+        return tuple(int(hex_color[i:i + 2], 16) / 255.0 for i in (0, 2, 4))
+
+
+class CustomTemplate(SimpleDocTemplate):
+    """ 定义页面模板样式 """
+
     def __init__(self, filename, **kw):
         super().__init__(filename, **kw)
-        template = PageTemplate('normal', [Frame(20 * mm, 23 * mm, 170 * mm, 250 * mm, id='F1')])
+        template = PageTemplate('normal', [Frame(0, 0, A4[0], A4[1], id='F1')])
         self.addPageTemplates(template)
 
     def afterFlowable(self, flowable):
         if flowable.__class__.__name__ == 'Paragraph':
             text = flowable.getPlainText()
             style = flowable.style.name
-            if style == 'Heading1':
-                level = 0
-            elif style == 'Heading2':
-                level = 1
-            elif style == 'Heading3':
-                level = 2
-            else:
-                return
-            e = [level, text, self.page]
-            bn = getattr(flowable, '_bookmarkName', None)
-            if bn is not None: e.append(bn)
-            self.notify('TOCEntry', tuple(e))
+            if style in ['Heading1', 'Heading2', 'Heading3']:
+                level = None
+                if style == 'Heading1':
+                    level = 0
+                elif style == 'Heading2':
+                    level = 1
+                elif style == 'Heading3':
+                    level = 2
+
+                # 调整页码，如果有封面则减1
+                adjusted_page_num = self.page - 1 if Cover else self.page
+
+                e = [level, text, adjusted_page_num]
+                bn = getattr(flowable, '_bookmarkName', None)
+                if bn is not None: e.append(bn)
+                self.notify('TOCEntry', tuple(e))
 
 
-# 定义页面页码样式
 class NumberPageCanvas(Canvas):
+    """ 定义页面页码样式 """
+
     def __init__(self, *args, **kwargs):
         Canvas.__init__(self, *args, **kwargs)
         self.pages = 0  # 初始化页面计数器
+        self.has_cover = Cover  # 假设第一页是封面
 
     def showPage(self):
-        self.pages += 1  # 页面计数器加一
-        self.draw_page_number(self.pages)  # 为当前页面添加页码
+        # 仅当不是封面时，增加页面计数器
+        if not self.has_cover:
+            self.pages += 1
+            self.draw_page_number(self.pages)
+        else:
+            self.has_cover = False  # 第一页之后，取消封面标记
         Canvas.showPage(self)
 
     def draw_page_number(self, page_number):
         # 绘制阴影矩形
         self.setFillColor("#808080")
-        self.rect(180 * mm, 10 * mm, 12 * mm, 6 * mm, fill=True, stroke=False)
+        self.rect(510, 28, 35, 17, fill=True, stroke=False)
         # 绘制间隙矩形
         self.setFillColor("#FFFFFF")
-        self.rect(180 * mm, 10.5 * mm, 12 * mm, 6 * mm, fill=True, stroke=False)
+        self.rect(510, 30, 35, 17, fill=True, stroke=False)
         # 绘制主矩形
         self.setFillColor("#2AB4E9")
-        self.rect(180 * mm, 11.5 * mm, 12 * mm, 6 * mm, fill=True, stroke=False)
+        self.rect(510, 32, 35, 17, fill=True, stroke=False)
         # 设置页码文字样式（占两位字符）
         self.setFillColor("#FFFFFF")
-        self.setFont("Helvetica-Bold", 14)
-        self.drawString(183.5 * mm, 13 * mm, "{:02d}".format(page_number))
-
-
-# pdf页面
-Pages = []
+        self.setFont("ChineseFont-Bold", 14)
+        self.drawString(520, 36, "{:02d}".format(page_number))
 
 
 # 函数接受数据参数并生成PDF
@@ -117,19 +201,32 @@ def build(filename: str) -> None:
     生成pdf
     :param filename: 指定文件路径文件名
     """
+    doc = []
     toc = TableOfContents()
     toc.levelStyles = ContentsStyle
     toc.dotsMinLevel = 0
-    Pages.insert(0, Paragraph('目录', ContentStyle))
-    Pages.insert(1, toc)
-    Pages.insert(2, PageBreak())
+    doc.extend(Cover)
+    doc.append(Paragraph('目录', ContentStyle))
+    doc.append(toc)
+    doc.append(PageBreak())
+    doc.extend(Pages)
     pdf = CustomTemplate(filename, pagesize=pagesizes.A4)
-    pdf.multiBuild(Pages, canvasmaker=NumberPageCanvas)
+    pdf.multiBuild(doc, canvasmaker=NumberPageCanvas)
+
+
+def addCover(coverPath: str, info: tuple) -> None:
+    """ 添加封面
+    :param coverPath: 封面路径
+    :param info: 封面信息
+    """
+    Cover.append(CanvasDrawing(coverPath, info))
+    Cover.append(PageBreak())
 
 
 # 设置页面内容
 def addText(body: List[dict[str, str]]) -> None:
-    """ 添加文本内容
+    """ 添加文本内容、List[dict]格式指明标题等级1、2、3为标题、4：为正文
+    例：body=[{'level': 1, 'content': "一、一级标题"}]
     :param body: 文本内容
     """
     Pages.extend(contents(body))
@@ -145,12 +242,16 @@ def addTable(header: str, table: List[dict], addSubRow: bool = False, columnBold
     :param conditions: 指定列数据条件判断
     :param merge: 指定列合并单元格
     """
-    Pages.append(
-        insertTable(header, table, addSubRow=addSubRow, columnBold=columnBold, conditions=conditions, merge=merge))
+    Pages.append(insert_table(header, table, add_sub_row=addSubRow,
+                              column_bold=columnBold, conditions=conditions, merge=merge))
 
 
-# 生成标题和目录
-def contents(title):
+def contents(title) -> List[Paragraph]:
+    """
+    生成目录
+    :param title: 目录内容
+    :return: 样式列表
+    """
     return [
         heading(item.get('content'),
                 h1 if item.get('level') == 1 else h2 if item.get('level') == 2 else h3 if item.get(
@@ -159,67 +260,74 @@ def contents(title):
     ]
 
 
-# 生成目录和标题关联链接
-def heading(text, style):
+#
+def heading(text, style) -> Paragraph:
+    """
+    生成目录和标题关联链接
+    :param text: 标题内容
+    :param style: 样式
+    :return:
+    """
     bn = sha1((text + style.name).encode()).hexdigest()
     h = Paragraph(text + '<a name="%s"/>' % bn, style)
     h._bookmarkName = bn
     return h
 
 
-# 插入表格内容
-def insertTable(header, data, addSubRow=False, columnBold: List[str] = None, conditions=None, merge=None):
+def insert_table(header, data, add_sub_row=False, column_bold: List[str] = None, conditions=None, merge=None) -> Table:
+    """
+    插入表格内容
+    :param header: 表格表头
+    :param data: 表格数据
+    :param add_sub_row: 是否自动添加次表头
+    :param column_bold: 指定列加粗
+    :param conditions: 指定列数据条件判断
+    :param merge: 指定列合并单元格
+    :return: 表格
+    """
     rows = []
     # 次行表头、表头、列表数据
-    subRow = list(data[0].keys())
-    headerRow = [header] + [""] * (len(subRow) - 1)
+    sub_row = list(data[0].keys())
+    header_row = [header] + [""] * (len(sub_row) - 1)
     items = pd.DataFrame(data).values.tolist()
     # 保存一份原始数据，用于cellStyle、mergeCells
-    originalSub = [row for row in subRow]
-    originalItems = [list(row) for row in items]
+    original_sub = [row for row in sub_row]
+    original_items = [list(row) for row in items]
 
     # 组装次行表头、列表数据
-    if addSubRow:
-        rows.append(subRow)
+    if add_sub_row:
+        rows.append(sub_row)
     rows.extend(items)
 
     # 定义次表头样式，指定列加粗
     columns = None
-    if columnBold is not None:
-        columns = [originalSub.index(i) for i in columnBold if i in originalSub]
+    if column_bold is not None:
+        columns = [original_sub.index(i) for i in column_bold if i in original_sub]
     # 处理数据自动换行
-    autoWrap(rows, isSubBold=addSubRow, columnBold=columns)
+    auto_wrap(rows, is_sub_bold=add_sub_row, column_bold=columns)
 
     # 创建表格, 固定首行行高，其余行行高自适应
-    table = Table([headerRow] + rows, rowHeights=([27] + [None] * (len(rows))), colWidths=170 * mm / len(subRow))
+    table = Table([header_row] + rows, rowHeights=([27] + [None] * (len(rows))), colWidths=480 / len(sub_row))
     # 设置全局表格样式
     table.setStyle(tableStyle)
-    cellStyle(table, conditions, originalSub, originalItems, hasSubheader=addSubRow)
-    mergeCells(table, merge, originalSub, originalItems)
+    cell_style(table, conditions, original_sub, original_items, has_sub_header=add_sub_row)
+    merge_cells(table, merge, original_sub, original_items)
     return table
 
 
-specifyColumns = PS(name='specifyColumns', fontName='ChineseFont-Bold', fontSize=10)
-
-
-def autoWrap(rows, isSubBold=False, columnBold=None):
+def auto_wrap(rows, is_sub_bold=False, column_bold=None) -> None:
     """
     自动换行并为指定列应用固定样式，首行有固定样式。
-
-    :param rows: 表格的行数据，每行是一个列表。
-    :param styled_columns: 应用特定样式的列索引列表。
+    :param rows: 表格的行数据，每行是一个列表
+    :param is_sub_bold: 次行表头是否加粗
+    :param column_bold: 指定列加粗
     """
-    styles = getSampleStyleSheet()
-    subRowStyle = styles['Normal'].clone(name='subRowStyle', fontName='ChineseFont-Bold', fontSize=10)
-    columnStyles = styles['Normal'].clone(name='subRowStyle', fontName='ChineseFont-Bold', fontSize=10)
-    cellStyle = styles['Normal'].clone(name='cellStyle', fontName='ChineseFont', fontSize=10)
-
     for row_index, row in enumerate(rows):
         for column_index, item in enumerate(row):
             # 为首行和指定列应用特定样式
-            if isSubBold and row_index == 0:
+            if is_sub_bold and row_index == 0:
                 style = subRowStyle
-            elif columnBold and column_index in columnBold:
+            elif column_bold and column_index in column_bold:
                 style = columnStyles
             else:
                 style = cellStyle
@@ -227,46 +335,60 @@ def autoWrap(rows, isSubBold=False, columnBold=None):
                 row[column_index] = Paragraph(str(item), style)
 
 
-# 定义单元格样式
-def cellStyle(table, conditions, sub_row, items, hasSubheader=True):
+#
+def cell_style(table, conditions, sub_row, items, has_sub_header=True) -> None:
+    """
+    定义单元格样式
+    :param table: 表格
+    :param conditions: 条件列表
+    :param sub_row: 次级表头
+    :param items: 数据
+    :param has_sub_header: 是否包含表头
+    """
     if conditions is None:
         return
-    offset = 2 if hasSubheader else 1
+    offset = 2 if has_sub_header else 1
     for element in conditions:
-        errorColumn: bool = True
+        error_column: bool = True
         for i, item in enumerate(items):
             for j, cell in enumerate(item):
-                if errorColumn and sub_row[j] == element.get('column'):
+                if error_column and sub_row[j] == element.get('column'):
                     try:
                         if element.get('expression')(cell):
                             table.setStyle(
                                 TableStyle([('BACKGROUND', (j, i + offset), (j, i + offset), element.get('color'))]))
                     except Exception as e:
                         # 设置判断列表达式False，跳出该表达式的所有循环
-                        errorColumn = False
+                        error_column = False
                         logging.error(f"conditions中的表达式有误: {e}")
 
 
-# 处理合并单元格
-def mergeCells(table, merge, sub_row, items):
+def merge_cells(table, merge, sub_row, items) -> None:
+    """
+    处理合并单元格
+    :param table: 表格
+    :param merge: 指定列合并单元格
+    :param sub_row: 次行表头
+    :param items: 数据
+    """
     if merge is None:
         return
-    for mergeColumn in merge:
-        if mergeColumn in sub_row:
-            columnIndex = sub_row.index(mergeColumn)
-            startRow = None
+    for merge_column in merge:
+        if merge_column in sub_row:
+            column_index = sub_row.index(merge_column)
+            start_row = None
             prev_value = 'unique_nonexistent_value'  # 初始值设为一个唯一的值
-            for rowIndex, row_data in enumerate(items):
-                currentValue = row_data[columnIndex]
+            for row_index, row_data in enumerate(items):
+                current_value = row_data[column_index]
                 # 如果当前值是 None，将其视为一个特殊标记
-                if currentValue is None:
-                    currentValue = 'special_null_value'
-                if currentValue != prev_value:
-                    if startRow is not None and rowIndex - startRow > 1:
+                if current_value is None:
+                    current_value = 'special_null_value'
+                if current_value != prev_value:
+                    if start_row is not None and row_index - start_row > 1:
                         table.setStyle(
-                            TableStyle([('SPAN', (columnIndex, startRow + 2), (columnIndex, rowIndex + 1))]))
-                    startRow = rowIndex
-                prev_value = currentValue
+                            TableStyle([('SPAN', (column_index, start_row + 2), (column_index, row_index + 1))]))
+                    start_row = row_index
+                prev_value = current_value
             # 检查并处理最后一个合并区域
-            if startRow is not None and len(items) - startRow > 1:
-                table.setStyle(TableStyle([('SPAN', (columnIndex, startRow + 2), (columnIndex, len(items) + 1))]))
+            if start_row is not None and len(items) - start_row > 1:
+                table.setStyle(TableStyle([('SPAN', (column_index, start_row + 2), (column_index, len(items) + 1))]))

e795db4d1d2750656025311500c82f1833ee5761
cat <fayfoxcat@gamil.com>
2024-01-18 10:33:40 +0800

    暴力破解ssh

---
+++ src/base/script/sb2.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+import requests
+from concurrent.futures import ThreadPoolExecutor
+
+url = "https://112.124.67.183"
+
+
+# 定义发送请求的函数
+def send_request(url):
+    while True:
+        response = requests.get(url)
+        print(response.text)
+
+
+# # 使用ThreadPoolExecutor创建线程池
+# with ThreadPoolExecutor(max_workers=1) as executor:
+#     # 启动多个线程同时发送请求
+#     for _ in range(1):  # 这里设置为5个线程，您可以根据需求调整
+#         executor.submit(send_request, url)
+
+send_request(url)
